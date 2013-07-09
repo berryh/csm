413a4a80b25579aa5faacc6a2bbf0922941fa4f2
berryh <berryh@teamdestroy.tk>
2013-07-09 23:35:21 +0200

    Initial Commit

---
+++ index.php
@@ -0,0 +1,70 @@
+<?php ?>
+
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width-device-width, initial-scale=1.0" />
+		<title>Combi Service Meeuwes</title>
+		<link rel="stylesheet" href="css/bootstrap-slate.css" />
+		<link rel="stylesheet" href="css/bootstrap-responsive.css" />
+	</head>
+	<body>
+
+		<div class="navbar navbar-fixed-top">
+			<div class="navbar-inner">
+				<div class="container">
+					<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"> <span class="icon-th-list"></span> </a>
+					<a href="#" class="brand">Combi Service Meeuwes</a>
+					<div class="nav-collapse collapse">
+						<ul class="nav pull-right">
+							<li class="active">
+								<a href="index.php">Home</a>
+							</li>
+							<li >
+								<a href="index.php">Projecten</a>
+							</li>
+							<li>
+								<a href="index.php">Fotoreportage</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="hero-unit">
+			<h1>Combi Service Meeuwes<img align="right" src="/combiservicemeeuwes/img/Logo.png" width=25%/></h1>
+			<br />
+			<br />
+		</div>
+
+		<div class="container">
+			<div class="row-fluid">
+				<div class="span12">
+					<p>
+						Hier Komt denk ik wat tekst met wat info en een fotoreportage. Of helemaal niks.
+					</p>
+				</div>
+			</div>
+			<div class="row-fluid">
+				<div class="span6" align="center">
+					<h4>Over Ons</h4>
+					<p>
+						Hier komt wat over ons.
+					</p>
+				</div>
+				<div class="span6" align="center">
+					<h4>Contact</h4>
+					<p>
+						Hier Komt contactinformatie
+					</p>
+				</div>
+			</div>
+		</div>
+
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+		<script src="js/bootstrap.js"></script>
+
+	</body>
+
+</html>
